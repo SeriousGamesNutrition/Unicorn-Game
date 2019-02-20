b5f38ba54905ee2bf0f9d31e8fd03b82ad0a131b
trumtrum <trumtrum@trumtrum-pc.wireless-1x.unc.edu>
2019-02-20 13:23:54 -0500

    added the assets of the horse/unicorn/etc. (pawer pack) and the assets that weren't in there before from the 2d default that made it so it wouldn't compile (oops on my part with the download)
    added the assets of the horse/unicorn/etc. (pawer pack) and the assets that weren't in there before from the 2d default that made it so it wouldn't compile (oops on my part with the download)

---
+++ Assets/GameInteractionController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInteractionController : MonoBehaviour
+{
+    public int collectedValueSum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("character hit "+other.gameObject.name);
+
+        if (other.gameObject.name.Contains("Coin")){
+            collectedValueSum = collectedValueSum + other.gameObject.GetComponent<coinScript>().coinValue;
+            Debug.Log("I hit a coin");
+            Debug.Log("coin is worth: "+other.gameObject.GetComponent<coinScript>().coinValue);
+            Destroy(other.gameObject);
+            Debug.Log("Stored Value: "+ collectedValueSum);
+        }
+    }
+}
---
+++ Assets/coinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinScript : MonoBehaviour
+{
+    public int coinValue=10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("ball hit "+other.gameObject.name);
+
+        
+    }
+}
